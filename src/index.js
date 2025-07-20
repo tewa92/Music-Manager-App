921d84e71dbf7a08a21d5e4523dc45606ca11c6c
tewa92 <tewachewsaleigizer@gmail.com>
2025-07-21 02:47:06 +0300

    added provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 //import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
+            <App />
 
-        <App />
+        </Provider>
 
     </React.StrictMode>
 );
